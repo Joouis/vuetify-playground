43a8c44153dc72ab385333f33d6be7aa61f428e3
Joouis <luckyjoou@gmail.com>
2019-12-31 03:54:32 +0800

    Init demo of ui-components-list.

--- src/App.vue
+++ src/App.vue
@@ -25,32 +25,25 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Vuetify Mobile Playground</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <UIComponentsList />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import UIComponentsList from './components/UIComponentsList';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    UIComponentsList,
   },
 
   data: () => ({
---
+++ src/components/UIComponentsList.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card
+    max-width="500"
+    class="mx-auto"
+  >
+    <v-list>
+      <v-subheader>UI Components</v-subheader>
+      <template v-for="item in menuItems">
+        <template v-if="typeof item.items === 'object'">
+          <v-list-group :key="item.title" no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-title v-text="item.title" />
+            </template>
+            <v-list-item
+                v-for="subItem in item.items"
+                :key="subItem.title"
+              >
+                <v-list-item-content>
+                  <v-list-item-title v-text="subItem.title" />
+                </v-list-item-content>
+              </v-list-item>
+          </v-list-group>
+        </template>
+        <template v-else>
+          <v-list-item :key="item.title">
+            <v-list-item-icon :key="item.title" />
+            <v-list-item-title v-text="item.title" />
+          </v-list-item>
+        </template>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'UIComponentsList',
+
+  data () {
+    return {
+      menuItems: [
+        {
+          title: 'Alerts'
+        }, {
+          title: 'Avatars'
+        }, {
+          title: 'Badges'
+        }, {
+          title: 'Banners'
+        }, {
+          title: 'Bars',
+          items: [
+            {
+              title: 'App Bars'
+            }, {
+              title: 'Toolbars'
+            }, {
+              title: 'System bars'
+            }]
+        }
+      ],
+    }
+  }
+};
+</script>
