2806901193b1c49a8b1f26c8f4bd956360014016
Joouis <luckyjoou@gmail.com>
2020-01-02 01:22:40 +0800

    Init publish to github page flow.

---
+++ scripts/publish.js
@@ -0,0 +1,11 @@
+const ghpages = require('gh-pages');
+
+console.log('Start to publish...');
+
+ghpages.publish('dist', err => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+console.log('Published!');
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: [
+    'vuetify'
+  ],
+  publicPath: '/vuetify-mobile-playground'
+};
