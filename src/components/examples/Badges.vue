10b09eadbca64c2d46c53bafbf26c2a3cedd877a
Joouis <luckyjoou@gmail.com>
2020-01-05 21:03:45 +0800

    Init badges examples.

---
+++ src/components/examples/Badges.vue
@@ -0,0 +1,35 @@
+<template>
+  <ExampleWrapper>
+    <v-subheader>Tabs</v-subheader>
+    <Tabs />
+    <v-subheader>Show-on-hover</v-subheader>
+    <ShowOnHover />
+    <v-subheader>Dynamic Notifications</v-subheader>
+    <DynamicNotifications />
+    <v-subheader>Customization Options</v-subheader>
+    <CustomizationOptions />
+  </ExampleWrapper>
+</template>
+
+<script>
+import ExampleWrapper from './ExampleWrapper';
+import Tabs from './badges/Tabs';
+import ShowOnHover from './badges/ShowOnHover';
+import DynamicNotifications  from './badges/DynamicNotifications';
+import CustomizationOptions from './badges/CustomizationOptions';
+
+// FIXME: badges do not show up.
+export default {
+  name: 'Badges',
+  
+  components: {
+    ExampleWrapper,
+    Tabs,
+    ShowOnHover,
+    DynamicNotifications,
+    CustomizationOptions
+  },
+
+  data: () => ({})
+};
+</script>
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -4,13 +4,15 @@ import VueRouter from 'vue-router';
 import UIComponentsList from '../components/UIComponentsList.vue';
 import Alerts from '../components/examples/Alerts';
 import Avatars from '../components/examples/Avatars';
+import Badges from '../components/examples/Badges';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: UIComponentsList },
   { path: '/alerts', component: Alerts },
-  { path: '/avatars', component: Avatars }
+  { path: '/avatars', component: Avatars },
+  { path: '/badges', component: Badges }
 ];
 
 export default new VueRouter({ routes });
