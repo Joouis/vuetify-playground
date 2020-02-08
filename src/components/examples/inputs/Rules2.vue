98cad7a563d14a4787f605fa0a935457e99f8860
Joouis <luckyjoou@gmail.com>
2020-02-08 21:46:48 +0800

    Fix warning log in Inputs page.

--- src/components/examples/inputs/Rules2.vue
+++ src/components/examples/inputs/Rules2.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-text-field label="Main input" :rules="rules" hide-details="auto"></v-text-field>
+    <v-text-field label="Main input" :rules="rules" hide-details></v-text-field>
     <v-text-field label="Another input"></v-text-field>
   </div>
 </template>
